afd35626ab3ed5147657f573f15c6ce084ac6f02
sweetharwest <truan35@yndex.ru>
2024-09-06 11:48:41 +0300

    update

--- src/components/main-page/MainPage.js
+++ src/components/main-page/MainPage.js
@@ -1,92 +1,115 @@
 import React, { useState, useEffect } from 'react';
-import { BarChart, LineChart, Pie } from 'react-chartjs-2';
+import { Bar, Line, Pie } from 'react-chartjs-2';
+import 'chart.js/auto';
 import './MainPage.css';
 
 const MainPage = (initialPeople) => {
 
-    const [chartData, setChartData] = useState({});
+    const [chartData, setChartData] = useState({
+        sex: {
+            labels: [],
+            datasets: [{
+                label: '',
+                data: [],
+            }],
+        },
+        age: {
+            labels: [],
+            datasets: [{
+                label: 'Age Distribution',
+                data: [],
+                backgroundColor: 'rgba(255, 206, 86, 0.2)',
+                borderColor: 'rgba(255, 206, 86, 1)',
+                borderWidth: 1,
+            }],
+        },
+        salary: {
+            labels: [],
+            datasets: [{
+                label: 'Salary Distribution',
+                data: [],
+                backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                borderColor: 'rgba(75, 192, 192, 1)',
+                borderWidth: 1,
+            }],
+        },
+    });
+    const [sexData, setSexData] = useState({});
 
     useEffect(() => {
-        /*const sexData = initialPeople.initialPeople.map(person => person.personalInformation.sex).reduce((acc, sex) => {
-            acc[sex] = (acc[sex] || 0) + 1;
-            return acc;
-        }, {});*/
-
         const sexData = initialPeople.initialPeople.reduce((acc, person) => {
             acc[person.personalInformation.sex] = (acc[person.personalInformation.sex] || 0) + 1;
             return acc;
         }, {});
+        setSexData(sexData);
 
-        alert(Object.keys(sexData));
-        alert(Object.values(sexData));
-
-        //alert(Object.keys(initialPeople));
-        /*const sexData = penis.map(person => person.personalInformation.sex).reduce((acc, sex) => {
-            acc[sex] = (acc[sex] || 0) + 1;
-            return acc;
-        }, {});*/
-
-
-        /*const ageData = initialPeople.reduce((acc, person) => {
-            acc[person.personalInformation.age] = (acc[person.age] || 0) + 1;
+        const ageData = initialPeople.initialPeople.reduce((acc, person) => {
+            let age = Math.floor((new Date() - person.personalInformation.birthDate)/31557600000);
+            acc[age] = (acc[age] || 0) + 1;
             return acc;
         }, {});
 
-        const salaryData = peopleData.reduce((acc, person) => {
-            acc[person.salary] = (acc[person.salary] || 0) + 1;
+        const salaryData = initialPeople.initialPeople.reduce((acc, person) => {
+            acc[person.job.salary] = (acc[person.job.salary] || 0) + 1;
             return acc;
-        }, {});*/
+        }, {});
 
         setChartData({
             sex: {
-                labels: ['a', 'b'],
+                labels: Object.keys(sexData),
                 datasets: [{
-                    label: 'Sex Distribution',
-                    data: [3, 4],
+                    label: 'Распределение по полу',
+                    data: Object.values(sexData),
                     backgroundColor: [
-                        'rgba(255, 99, 132, 0.2)',
                         'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 99, 132, 0.2)',
                     ],
                     borderColor: [
-                        'rgba(255, 99, 132, 1)',
                         'rgba(54, 162, 235, 1)',
+                        'rgba(255, 99, 132, 1)',
                     ],
                     borderWidth: 1,
                 }],
             },
-            /*age: {
+            age: {
                 labels: Object.keys(ageData),
                 datasets: [{
-                    label: 'Age Distribution',
+                    label: 'Распределение по возрасту',
                     data: Object.values(ageData),
                     backgroundColor: 'rgba(255, 206, 86, 0.2)',
                     borderColor: 'rgba(255, 206, 86, 1)',
                     borderWidth: 1,
                 }],
-            },*/
-            /*salary: {
+            },
+            salary: {
                 labels: Object.keys(salaryData),
                 datasets: [{
-                    label: 'Salary Distribution',
+                    label: 'Рапределение по зарплате',
                     data: Object.values(salaryData),
                     backgroundColor: 'rgba(75, 192, 192, 0.2)',
                     borderColor: 'rgba(75, 192, 192, 1)',
                     borderWidth: 1,
                 }],
-            },*/
+            },
         });
     }, [initialPeople]);
 
     return (
-        <div>
-            <h2>Gender Distribution</h2>
-            <Pie data={chartData.sex} />
+        <div className="main-page-container">
+            <div className="sex-distribution-container">
+                <Pie className="sex-distribution-pie" data={chartData.sex}/>
+                <div className="sex-distribution-content">
+                    Распределение по полу:
+                    <p>Мужчин: {Object.values(sexData)[0]}</p>
+                    <p>Женщин: {Object.values(sexData)[1]}</p>
+                </div>
+            </div>
 
-            {/*<h2>Age Distribution</h2>
-            <BarChart data={chartData.age} />
+            <h2>Распределение по возрасту</h2>
+            <Bar data={chartData.age}/>
 
-            <h2>Salary Distribution</h2>
-            <LineChart data={chartData.salary} />*/}
+            <h2>Рапределение по зарплате</h2>
+            <Line data={chartData.salary}/>
         </div>
     );
 }
